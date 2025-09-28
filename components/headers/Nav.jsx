992e3b3df8a6dc65f82ad3db07fdf896c5dc1d78
sujon3537 <119605414+sujon3537@users.noreply.github.com>
2025-09-28 11:42:49 +0600

    test

--- components/headers/Nav.jsx
+++ components/headers/Nav.jsx
@@ -3,281 +3,276 @@ import Link from "next/link";
 import React, { useEffect } from "react";
 import Image from "next/image";
 import {
-   blockItems,
-   blogItems,
-   demos,
-   docsPages,
-   otherPages,
-   projectPages,
+  blockItems,
+  blogItems,
+  demos,
+  docsPages,
+  otherPages,
+  projectPages,
 } from "@/data/menu";
 import { usePathname } from "next/navigation";
 // !text-[var(--current-color)]
 export default function Nav({ color = "#fab758" }) {
-   useEffect(() => {
-      // Dynamically import Bootstrap
-      import("bootstrap").then((Bootstrap) => {
-         const CLASS_NAME = "has-child-dropdown-show";
+  useEffect(() => {
+    // Dynamically import Bootstrap
+    import("bootstrap").then((Bootstrap) => {
+      const CLASS_NAME = "has-child-dropdown-show";
 
-         // Save the original toggle function
-         const originalToggle = Bootstrap.Dropdown.prototype.toggle;
+      // Save the original toggle function
+      const originalToggle = Bootstrap.Dropdown.prototype.toggle;
 
-         // Override the toggle function
-         Bootstrap.Dropdown.prototype.toggle = function () {
-            // Remove the CLASS_NAME from all dropdowns
-            document.querySelectorAll("." + CLASS_NAME).forEach(function (e) {
-               e.classList.remove(CLASS_NAME);
-            });
+      // Override the toggle function
+      Bootstrap.Dropdown.prototype.toggle = function () {
+        // Remove the CLASS_NAME from all dropdowns
+        document.querySelectorAll("." + CLASS_NAME).forEach(function (e) {
+          e.classList.remove(CLASS_NAME);
+        });
 
-            // Traverse up through the closest dropdown parents
-            let dd = this._element
-               .closest(".dropdown")
-               .parentNode.closest(".dropdown");
-            for (; dd && dd !== document; dd = dd.parentNode.closest(".dropdown")) {
-               dd.classList.add(CLASS_NAME);
-            }
+        // Traverse up through the closest dropdown parents
+        let dd = this._element
+          .closest(".dropdown")
+          .parentNode.closest(".dropdown");
+        for (; dd && dd !== document; dd = dd.parentNode.closest(".dropdown")) {
+          dd.classList.add(CLASS_NAME);
+        }
 
-            // Call the original toggle function
-            return originalToggle.call(this);
-         };
-
-         // Add event listeners for hide.bs.dropdown to remove the CLASS_NAME
-         document.querySelectorAll(".dropdown").forEach(function (dd) {
-            dd.addEventListener("hide.bs.dropdown", function (e) {
-               if (this.classList.contains(CLASS_NAME)) {
-                  this.classList.remove(CLASS_NAME);
-                  e.preventDefault();
-               }
-               e.stopPropagation();
-            });
-         });
-      });
-
-      // Optional cleanup function for any potential side effects
-      return () => {
-         // Cleanup code here (if needed)
+        // Call the original toggle function
+        return originalToggle.call(this);
       };
-   }, []);
 
-   const pathname = usePathname();
-
-   const getActiveParent = (menuLinks) => {
-      return menuLinks.find((parent) => {
-         if (parent.links) {
-            return parent.links.some((link) => link.href === pathname);
-         }
-         return parent.href === pathname;
+      // Add event listeners for hide.bs.dropdown to remove the CLASS_NAME
+      document.querySelectorAll(".dropdown").forEach(function (dd) {
+        dd.addEventListener("hide.bs.dropdown", function (e) {
+          if (this.classList.contains(CLASS_NAME)) {
+            this.classList.remove(CLASS_NAME);
+            e.preventDefault();
+          }
+          e.stopPropagation();
+        });
       });
-   };
+    });
 
-   return (
-      <ul className="navbar-nav" style={{ "--current-color": color }}>
-         <Link
+    // Optional cleanup function for any potential side effects
+    return () => {
+      // Cleanup code here (if needed)
+    };
+  }, []);
 
-            className={` nav-link hover:!text-[var(--current-color)]   ${pathname === "/"
-               ? "!text-[var(--current-color)]"
-               : ""
-               } `}
-            href="/"
+  const pathname = usePathname();
 
-         >
-            Home
-         </Link>
+  const getActiveParent = (menuLinks) => {
+    return menuLinks.find((parent) => {
+      if (parent.links) {
+        return parent.links.some((link) => link.href === pathname);
+      }
+      return parent.href === pathname;
+    });
+  };
 
-         <li className="nav-item dropdown">
-            <a
-               className={`nav-link dropdown-toggle !text-[.85rem] !tracking-[normal] hover:!text-[var(--current-color)] after:!text-[var(--current-color)] ${getActiveParent(otherPages) ? "!text-[var(--current-color)]" : ""
-                  } `}
-               href="#"
-               data-bs-toggle="dropdown"
-               aria-expanded="false"
+  return (
+    <ul className="navbar-nav" style={{ "--current-color": color }}>
+      {/* <Link
+        className={` nav-link hover:!text-[var(--current-color)]   ${
+          pathname === "/" ? "!text-[var(--current-color)]" : ""
+        } `}
+        href="/"
+      >
+        Home
+      </Link> */}
+
+      <li className="nav-item dropdown">
+        <a
+          className={`nav-link dropdown-toggle !text-[.85rem] !tracking-[normal] hover:!text-[var(--current-color)] after:!text-[var(--current-color)] ${
+            getActiveParent(otherPages) ? "!text-[var(--current-color)]" : ""
+          } `}
+          href="#"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          Services
+        </a>
+        <ul className="dropdown-menu">
+          {otherPages.map((item) => (
+            <li
+              key={item.id}
+              className={`dropdown ${
+                item.links ? "dropdown-submenu dropend" : "nav-item"
+              }`}
             >
-               Services
-            </a>
-            <ul className="dropdown-menu">
-               {otherPages.map((item) => (
-                  <li
-                     key={item.id}
-                     className={`dropdown ${item.links ? "dropdown-submenu dropend" : "nav-item"
-                        }`}
+              {item.links ? (
+                <>
+                  <a
+                    className={`dropdown-item hover:!text-[var(--current-color)] dropdown-toggle  ${
+                      getActiveParent([item])
+                        ? "!text-[var(--current-color)]"
+                        : ""
+                    } `}
+                    href="#"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
                   >
-                     {item.links ? (
-                        <>
-                           <a
-                              className={`dropdown-item hover:!text-[var(--current-color)] dropdown-toggle  ${getActiveParent([item])
-                                 ? "!text-[var(--current-color)]"
-                                 : ""
-                                 } `}
-                              href="#"
-                              data-bs-toggle="dropdown"
-                              aria-expanded="false"
-                           >
-                              {item.label}
-                           </a>
-                           <ul className="dropdown-menu submenu">
-                              {item.links.map((subItem) => (
-                                 <li key={subItem.id} className="nav-item">
-                                    <Link
-                                       className={`dropdown-item hover:!text-[var(--current-color)]   ${subItem.href == pathname
-                                          ? "!text-[var(--current-color)]"
-                                          : ""
-                                          } `}
-                                       href={subItem.href}
-                                    >
-                                       {subItem.label}
-                                    </Link>
-                                 </li>
-                              ))}
-                           </ul>
-                        </>
-                     ) : (
+                    {item.label}
+                  </a>
+                  <ul className="dropdown-menu submenu">
+                    {item.links.map((subItem) => (
+                      <li key={subItem.id} className="nav-item">
                         <Link
-                           className={`dropdown-item hover:!text-[var(--current-color)]   ${item.href == pathname ? "!text-[var(--current-color)]" : ""
-                              } `}
-                           href={item.href}
+                          className={`dropdown-item hover:!text-[var(--current-color)]   ${
+                            subItem.href == pathname
+                              ? "!text-[var(--current-color)]"
+                              : ""
+                          } `}
+                          href={subItem.href}
                         >
-                           {item.label}
+                          {subItem.label}
                         </Link>
-                     )}
-                  </li>
-               ))}
-            </ul>
-         </li>
-
-         <li className="nav-item dropdown">
-            <a
-               className={`nav-link dropdown-toggle !text-[.85rem] !tracking-[normal] hover:!text-[var(--current-color)] after:!text-[var(--current-color)] ${getActiveParent(otherPages) ? "!text-[var(--current-color)]" : ""
+                      </li>
+                    ))}
+                  </ul>
+                </>
+              ) : (
+                <Link
+                  className={`dropdown-item hover:!text-[var(--current-color)]   ${
+                    item.href == pathname ? "!text-[var(--current-color)]" : ""
                   } `}
-               href="#"
-               data-bs-toggle="dropdown"
-               aria-expanded="false"
-            >
-               Products
-            </a>
+                  href={item.href}
+                >
+                  {item.label}
+                </Link>
+              )}
+            </li>
+          ))}
+        </ul>
+      </li>
 
-            <ul className="dropdown-menu">
-               {otherPages.map((item) => (
-                  <li
-                     key={item.id}
-                     className={`dropdown ${item.links ? "dropdown-submenu dropend" : "nav-item"
-                        }`}
+      <li className="nav-item dropdown">
+        <a
+          className={`nav-link dropdown-toggle !text-[.85rem] !tracking-[normal] hover:!text-[var(--current-color)] after:!text-[var(--current-color)] ${
+            getActiveParent(otherPages) ? "!text-[var(--current-color)]" : ""
+          } `}
+          href="#"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          Products
+        </a>
+
+        <ul className="dropdown-menu">
+          {otherPages.map((item) => (
+            <li
+              key={item.id}
+              className={`dropdown ${
+                item.links ? "dropdown-submenu dropend" : "nav-item"
+              }`}
+            >
+              {item.links ? (
+                <>
+                  <a
+                    className={`dropdown-item hover:!text-[var(--current-color)] dropdown-toggle  ${
+                      getActiveParent([item])
+                        ? "!text-[var(--current-color)]"
+                        : ""
+                    } `}
+                    href="#"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
                   >
-                     {item.links ? (
-                        <>
-                           <a
-                              className={`dropdown-item hover:!text-[var(--current-color)] dropdown-toggle  ${getActiveParent([item])
-                                 ? "!text-[var(--current-color)]"
-                                 : ""
-                                 } `}
-                              href="#"
-                              data-bs-toggle="dropdown"
-                              aria-expanded="false"
-                           >
-                              {item.label}
-                           </a>
-                           <ul className="dropdown-menu submenu">
-                              {item.links.map((subItem) => (
-                                 <li key={subItem.id} className="nav-item">
-                                    <Link
-                                       className={`dropdown-item hover:!text-[var(--current-color)]   ${subItem.href == pathname
-                                          ? "!text-[var(--current-color)]"
-                                          : ""
-                                          } `}
-                                       href={subItem.href}
-                                    >
-                                       {subItem.label}
-                                    </Link>
-                                 </li>
-                              ))}
-                           </ul>
-                        </>
-                     ) : (
+                    {item.label}
+                  </a>
+                  <ul className="dropdown-menu submenu">
+                    {item.links.map((subItem) => (
+                      <li key={subItem.id} className="nav-item">
                         <Link
-                           className={`dropdown-item hover:!text-[var(--current-color)]   ${item.href == pathname ? "!text-[var(--current-color)]" : ""
-                              } `}
-                           href={item.href}
+                          className={`dropdown-item hover:!text-[var(--current-color)]   ${
+                            subItem.href == pathname
+                              ? "!text-[var(--current-color)]"
+                              : ""
+                          } `}
+                          href={subItem.href}
                         >
-                           {item.label}
+                          {subItem.label}
                         </Link>
-                     )}
-                  </li>
-               ))}
-            </ul>
-         </li>
-         <li className="nav-item dropdown">
-            <a
-               className={`nav-link dropdown-toggle !text-[.85rem] !tracking-[normal] hover:!text-[var(--current-color)] after:!text-[var(--current-color)]  ${getActiveParent(projectPages) ? "!text-[var(--current-color)]" : ""
+                      </li>
+                    ))}
+                  </ul>
+                </>
+              ) : (
+                <Link
+                  className={`dropdown-item hover:!text-[var(--current-color)]   ${
+                    item.href == pathname ? "!text-[var(--current-color)]" : ""
                   } `}
-               href="#"
-               data-bs-toggle="dropdown"
-            >
-               Projects
-            </a>
-            <div className="dropdown-menu dropdown-lg">
-               <div className="dropdown-lg-content">
-                  {projectPages.map((section) => (
-                     <div key={section.id}>
-                        <h6 className="dropdown-header !text-[var(--current-color)]">
-                           {section.title}
-                        </h6>
-                        <ul className="pl-0 list-none">
-                           {section.links.map((link) => (
-                              <li key={link.id}>
-                                 <Link
-                                    className={`dropdown-item hover:!text-[var(--current-color)]   ${link.href == pathname
-                                       ? "!text-[var(--current-color)]"
-                                       : ""
-                                       }  `}
-                                    href={link.href}
-                                 >
-                                    {link.label}
-                                 </Link>
-                              </li>
-                           ))}
-                        </ul>
-                     </div>
+                  href={item.href}
+                >
+                  {item.label}
+                </Link>
+              )}
+            </li>
+          ))}
+        </ul>
+      </li>
+      <li className="nav-item dropdown">
+        <a
+          className={`nav-link dropdown-toggle !text-[.85rem] !tracking-[normal] hover:!text-[var(--current-color)] after:!text-[var(--current-color)]  ${
+            getActiveParent(projectPages) ? "!text-[var(--current-color)]" : ""
+          } `}
+          href="#"
+          data-bs-toggle="dropdown"
+        >
+          Projects
+        </a>
+        <div className="dropdown-menu dropdown-lg">
+          <div className="dropdown-lg-content">
+            {projectPages.map((section) => (
+              <div key={section.id}>
+                <h6 className="dropdown-header !text-[var(--current-color)]">
+                  {section.title}
+                </h6>
+                <ul className="pl-0 list-none">
+                  {section.links.map((link) => (
+                    <li key={link.id}>
+                      <Link
+                        className={`dropdown-item hover:!text-[var(--current-color)]   ${
+                          link.href == pathname
+                            ? "!text-[var(--current-color)]"
+                            : ""
+                        }  `}
+                        href={link.href}
+                      >
+                        {link.label}
+                      </Link>
+                    </li>
                   ))}
-               </div>
-               {/* /auto-column */}
-            </div>
-         </li>
-         <Link
-
-            className={` nav-link hover:!text-[var(--current-color)]   ${pathname === "/company"
-               ? "!text-[var(--current-color)]"
-               : ""
-               } `}
-            href="/portfolio"
-
-         >
-            Portfolio
-         </Link>
-         <Link
-
-            className={` nav-link hover:!text-[var(--current-color)]   ${pathname === "/company"
-               ? "!text-[var(--current-color)]"
-               : ""
-               } `}
-            href="/company"
-
-         >
-            Company
-         </Link>
-         <Link
-
-            className={` nav-link hover:!text-[var(--current-color)]   ${pathname === "/blog"
-               ? "!text-[var(--current-color)]"
-               : ""
-               } `}
-            href="/blog"
-
-         >
-            Blog
-         </Link>
-
-
-      </ul>
-   );
+                </ul>
+              </div>
+            ))}
+          </div>
+          {/* /auto-column */}
+        </div>
+      </li>
+      <Link
+        className={` nav-link hover:!text-[var(--current-color)]   ${
+          pathname === "/company" ? "!text-[var(--current-color)]" : ""
+        } `}
+        href="/portfolio"
+      >
+        Portfolio
+      </Link>
+      <Link
+        className={` nav-link hover:!text-[var(--current-color)]   ${
+          pathname === "/company" ? "!text-[var(--current-color)]" : ""
+        } `}
+        href="/about-us"
+      >
+        About Us
+      </Link>
+      <Link
+        className={` nav-link hover:!text-[var(--current-color)]   ${
+          pathname === "/blog" ? "!text-[var(--current-color)]" : ""
+        } `}
+        href="/blog"
+      >
+        Blog
+      </Link>
+    </ul>
+  );
 }
-
-
-
--- data/socials.js
+++ data/socials.js
@@ -1,7 +1,27 @@
 export const socialLinks = [
-  { icon: "uil-twitter", unicode: "\\ed59", color: "#5daed5", href: "#" },
-  { icon: "uil-facebook-f", unicode: "\\eae2", color: "#4470cf", href: "#" },
-  { icon: "uil-dribbble", unicode: "\\eaa2", color: "#e94d88", href: "#" },
+  {
+    icon: "uil-twitter",
+    unicode: "\\ed59",
+    color: "#5daed5",
+    href: "https://x.com/webhivebd",
+  },
+  {
+    icon: "uil-facebook-f",
+    unicode: "\\eae2",
+    color: "#4470cf",
+    href: "https://www.facebook.com/webhivebd",
+  },
+  {
+    icon: "uil-linkedin",
+    unicode: "\\eaa2",
+    color: "#0077B5",
+    href: "https://www.linkedin.com/company/webhivebd",
+  },
   { icon: "uil-instagram", unicode: "\\eb9c", color: "#d53581", href: "#" },
-  { icon: "uil-youtube", unicode: "\\edb5", color: "#c8312b", href: "#" },
+  {
+    icon: "uil-youtube",
+    unicode: "\\edb5",
+    color: "#c8312b",
+    href: "https://www.youtube.com/@webhivebd",
+  },
 ];
